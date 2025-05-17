6db71bb2e4f46b3b44641763a11a80ad48c483c7
dungnanoboy <dungnanoboy@gmail.com>
2025-05-17 16:27:34 +0700

    First commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the 'public' directory inside 'src'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Nếu muốn chuyển hướng root "/" về file index.html trong public:
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
